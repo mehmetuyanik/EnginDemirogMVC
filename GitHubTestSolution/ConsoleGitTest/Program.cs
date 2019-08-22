96ac1caca355528f80b395dc85243ff047d3217a
Mehmet UYANIK <mehmetuyanik@outlook.com>
2019-08-22 05:05:57 +0300

    last commit test

--- GitHubTestSolution/ConsoleGitTest/Program.cs
+++ GitHubTestSolution/ConsoleGitTest/Program.cs
@@ -12,7 +12,6 @@ namespace ConsoleGitTest
         {
             Console.WriteLine("Second commit");
             Console.ReadLine();
-            Console.WriteLine("Find the differences");
 
         }
     }
